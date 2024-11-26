e52727dcd29e7240294d43d4b3eb1f9f284b0678
Marco <m_uno_p@hotmail.com>
2024-11-26 17:50:28 -0500

    Set the routes constraints for the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+  
+  # API definition
+  namespace :api, defaults: { format: :json } do
+    # We are going to list our resources here
+  end
 end
