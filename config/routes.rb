83f1ea527d2f06a7515f725b9b166e96e39f984c
Marco <m_uno_p@hotmail.com>
2024-11-26 18:15:44 -0500

    Use rubocop to fix a lint issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   # API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
