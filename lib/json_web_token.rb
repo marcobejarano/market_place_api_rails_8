85acd0fd122fc5b76b18d98be7902a4f7e610520
Marco <m_uno_p@hotmail.com>
2024-11-27 14:48:35 -0500

    Set up tokens controller

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,8 +1,8 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
   
-  def self.encode(paylod, exp=24.hours.from_now)
-    payload[:exp] = exp.to_id
+  def self.encode(payload, exp=24.hours.from_now)
+    payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
   
