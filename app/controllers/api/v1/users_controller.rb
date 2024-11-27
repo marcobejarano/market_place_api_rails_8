39179f8677db475c50073a6b7ed5da5a098d978e
Marco <m_uno_p@hotmail.com>
2024-11-27 11:45:38 -0500

    Add destroy action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,22 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[show update]
-  
+  before_action :set_user, only: %i[show update destroy]
+
   # GET /users/1
   def show
     render json: @user
   end
-  
+
   # POST /users
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-  
+
   # PUT /users
   def update
     if @user.update(user_params)
@@ -25,13 +25,19 @@ class Api::V1::UsersController < ApplicationController
       render josn: @user.errors, status: :unprocessable_entity
     end
   end
-  
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
   private
-  
+
   def set_user
     @user = User.find(params[:id])
   end
-  
+
   # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:email, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   # API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show create update]
+      resources :users, only: %i[show create update destroy]
     end
   end
 end
