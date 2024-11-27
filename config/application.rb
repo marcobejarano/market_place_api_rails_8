0276d5e986a512708b16d2e907a2744cef56c3ba
Marco <m_uno_p@hotmail.com>
2024-11-27 13:49:16 -0500

    Set up JWT gem

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module MarketPlaceApiRails8
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("lib")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
