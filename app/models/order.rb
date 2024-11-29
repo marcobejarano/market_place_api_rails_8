a0c1a9c785d0b0681ddf84fdae554986e1bfdfd9
Marco <m_uno_p@hotmail.com>
2024-11-29 13:36:42 -0500

    Add the create method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  before_validation :set_total!
+  after_commit :set_total!, on: [:create, :update]
+  
   validates :total,
     numericality: {
       greater_than_or_equal_to: 0
@@ -8,4 +11,8 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
+  
+  def set_total!
+    self.total = products.sum :price
+  end
 end
